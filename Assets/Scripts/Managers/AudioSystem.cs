d81a4bf253167256ec9ee221171f1e28342801f5
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2024-08-17 15:53:28 +0100

    draggable audio

--- Assets/Scripts/Managers/AudioSystem.cs
+++ Assets/Scripts/Managers/AudioSystem.cs
@@ -1,11 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioSystem : MonoBehaviour
 {
+    [Header("Music")]
     public AudioSource musicSource;
 
+    [Header("Drag")]
+    public List<AudioSource> dragSources = new List<AudioSource>();
+
+    public DraggableAudioClips dragAudioClips;
+
+
+    private void Start()
+    {
+        for (int i = 0; i < 10; i++) 
+        {
+            dragSources.Add(gameObject.AddComponent<AudioSource>());
+        }
+    }
     public void PlayMusic()
     {
+        if (musicSource == null || musicSource.isPlaying)
+            return;
+
         musicSource.Play();
+    }
+
+
+    public void PlayDragSelected()
+    {
+        foreach (AudioSource source in dragSources)
+        {
+            if (source != null && !source.isPlaying)
+            {
+                source.clip = dragAudioClips.dragSelect;
+                source.Play();
+                break;
+            }
+        }
+    }
+
+    public void PlayDragDeselected()
+    {
+        foreach (AudioSource source in dragSources)
+        {
+            if (source != null && !source.isPlaying) 
+            {
+                source.clip = dragAudioClips.dragDrop;
+                source.Play();
+                break;
+            }
+        }
     }
 }
