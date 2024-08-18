89097da99f6363e9265d5479e0c2311e13d6f630
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2024-08-18 14:41:29 +0100

    updated torch trigger object

--- Assets/Scripts/Trigger/TorchTriggerObject.cs
+++ Assets/Scripts/Trigger/TorchTriggerObject.cs
@@ -1,9 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 public class TorchTriggerObject : TriggerObject
 {
     public Light lightToSwitch;
     public bool startOn;
+
+    public bool switchOffAfterTime;
+    public float lightSwitchTime;
+
     private void Start()
     {
         if (startOn)
@@ -15,11 +20,31 @@ public class TorchTriggerObject : TriggerObject
             lightToSwitch.enabled = false;
         }
     }
+
     public override void Trigger()
+    {
+        if (switchOffAfterTime && lightSwitchTime > 0)
+        {
+            StartCoroutine(SwitchLight());
+        }
+        else
+        {
+            Switch();
+        }
+    }
+
+    private IEnumerator SwitchLight()
+    {
+        Switch();
+        yield return new WaitForSeconds(lightSwitchTime);
+        Switch();
+    }
+
+    private void Switch()
     {
         if (lightToSwitch.enabled)
         {
-        lightToSwitch.enabled = false;
+            lightToSwitch.enabled = false;
         }
         else
         {
